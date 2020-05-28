7e162d8d23243ce0b5c95e0146d2dddfe50b3e94
hjludwig <hjeremyludwig@gmail.com>
2020-05-27 21:37:22 -0400

    End of day
    May 27

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function calculate() {
-    var taxrate = parseFloat(document.getElementById("taxrate").value);
+function calculateTotal() {
+    var taxrate = parseFloat(document.getElementById("taxrate1").value);
     var price = parseFloat(document.getElementById("price").value);
     var total = price + taxrate / 100 * price;
-    document.getElementById("answer").innerHTML = total;
+    document.getElementById("afterTax").textContent = total.toFixed(2);
   }
+function calculateTax() {
+    var taxrate = parseFloat(document.getElementById("taxrate2").value);
+    var paid = parseFloat(document.getElementById("paid").value);
+    var price = paid / (taxrate/100 + 1) ;
+    var taxPaid = paid - price;
+    document.getElementById("itemPrice").textContent = price.toFixed(2);
+    document.getElementById("taxPaid").textContent = taxPaid.toFixed(2);
+}
+function testFunction() {
+    console.log("Test Complete");
+    document.getElementById("taxPaid").textContent = "Test Complete";
+}
